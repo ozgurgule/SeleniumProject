1ade5c517130dd27661316c294da096f783ba95f
OzgurGuler <ozgur.guler.20@hotmail.com>
2022-02-18 18:11:58 +0300

    Add second updata

--- src/test/java/helloTest.java
+++ src/test/java/helloTest.java
@@ -1,5 +1,6 @@
 public class helloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
-    }
+
+        System.out.println("About to share project on gitHub");}
 }
